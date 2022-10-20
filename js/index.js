2265383cc163bfd99b5da404f46bfa5dcc06e67e
Gurpreet Kaur <gkguraya5@gmail.com>
2022-10-19 21:42:39 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const skillset= document.getElementsByClassName("skillset")
+for(i=0;i<skillset.length;i++){
+    const skills = skillset[i].getElementsByTagName("div");
+    for(j=0;j<skills.length;j++){
+    let html = "<i style='color:#f8333c' class='fa fa-code'></i>&nbsp;";
+    skills[j].insertAdjacentHTML("afterbegin", html)
+}
+}
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
